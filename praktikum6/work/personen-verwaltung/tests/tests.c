448861e70bb3550bbd7716b90584b0f5c9f5bdc4
Edelmetall <chime.switzerland@gmail.com>
2022-04-11 18:55:22 +0200

    praktikum 6 draft

--- praktikum6/work/personen-verwaltung/tests/tests.c
+++ praktikum6/work/personen-verwaltung/tests/tests.c
@@ -35,85 +35,135 @@
 
 #define TRACE_INDENT "\n                " ///< allow for better stdout formatting in case of error
 
+#include "../src/person.h"
+#include "../src/list.h"
+
+node_t *anchor;
+
+person_t *seb;
+person_t *seb2;
+person_t *olivia;
+person_t *fabia;
+
 // setup & cleanup
-static int setup(void)
-{
-    remove_file_if_exists(OUTFILE);
-    remove_file_if_exists(ERRFILE);
+static int setup(void) {
+    // do nothing
     return 0; // success
 }
 
-static int teardown(void)
-{
+static int teardown(void) {
     // Do nothing.
     // Especially: do not remove result files - they are removed in int setup(void) *before* running a test.
     return 0; // success
 }
 
+static void beforeEach(void) {
+    anchor = malloc(sizeof(node_t));
+    anchor->next = anchor;
+
+    seb = malloc(sizeof(person_t));
+    strcpy(seb->name, "Brunner");
+    strcpy(seb->first_name, "Sebastian");
+    seb->age = 22;
+
+    seb2 = malloc(sizeof(person_t));
+    strcpy(seb2->name, "Brunner");
+    strcpy(seb2->first_name, "Sebastian");
+    seb2->age = 23;
+
+    olivia = malloc(sizeof(person_t));
+    strcpy(olivia->name, "Brunner");
+    strcpy(olivia->first_name, "Olivia");
+    olivia->age = 19;
+
+    fabia = malloc(sizeof(person_t));
+    strcpy(fabia->name, "Müller");
+    strcpy(fabia->first_name, "Fabia");
+    fabia->age = 27; // almost 30, jeez
+
+    remove_file_if_exists(OUTFILE);
+    remove_file_if_exists(ERRFILE);
+}
+
 // tests
-static void test_person_compare(void)
-{
-	// BEGIN-STUDENTS-TO-ADD-CODE
-	// arrange
+static void test_person_compare(void) {
+    // BEGIN-STUDENTS-TO-ADD-CODE
+    // arrange
+    beforeEach();
 
-	// act
-	CU_FAIL("missing test");
-	
-	// assert
-	
-	// END-STUDENTS-TO-ADD-CODE
+    // act & assert
+    CU_ASSERT_EQUAL(person_compare(seb, seb), 0);
+
+    CU_ASSERT_TRUE(person_compare(seb, seb2) < 0);
+    CU_ASSERT_TRUE(person_compare(seb2, seb) > 0);
+
+    CU_ASSERT_TRUE(person_compare(seb, olivia) > 0);
+    CU_ASSERT_TRUE(person_compare(olivia, seb) < 0);
+
+    CU_ASSERT_TRUE(person_compare(seb, fabia) < 0);
+    CU_ASSERT_TRUE(person_compare(fabia, seb) > 0);
+
+    // END-STUDENTS-TO-ADD-CODE
 }
 
-static void test_list_insert(void)
-{
-	// BEGIN-STUDENTS-TO-ADD-CODE
-	// arrange
+static void test_list_insert(void) {
+    // BEGIN-STUDENTS-TO-ADD-CODE
+    // arrange
+    beforeEach();
 
-	// act
-	CU_FAIL("missing test");
-	
-	// assert
-	
-	// END-STUDENTS-TO-ADD-CODE
+    // act
+    int result = insert_node(anchor, seb);
+
+    // assert
+    CU_ASSERT_EQUAL(result, 1);
+    CU_ASSERT_EQUAL(anchor->next->content, seb);
+
+    // END-STUDENTS-TO-ADD-CODE
 }
 
-static void test_list_remove(void)
-{
-	// BEGIN-STUDENTS-TO-ADD-CODE
-	// arrange
+static void test_list_remove(void) {
+    // BEGIN-STUDENTS-TO-ADD-CODE
+    // arrange
+    beforeEach();
+    insert_node(anchor, seb);
+    insert_node(anchor, olivia);
 
-	// act
-	CU_FAIL("missing test");
-	
-	// assert
-	
-	// END-STUDENTS-TO-ADD-CODE
+    // act
+    int result = remove_node(anchor, seb);
+    int result2 = remove_node(anchor, fabia);
+
+    // assert
+    CU_ASSERT_EQUAL(result, 1);
+    CU_ASSERT_EQUAL(result2, 0);
+    CU_ASSERT_EQUAL(anchor->next->content, olivia);
+
+    // END-STUDENTS-TO-ADD-CODE
 }
 
-static void test_list_clear(void)
-{
-	// BEGIN-STUDENTS-TO-ADD-CODE
-	// arrange
+static void test_list_clear(void) {
+    // BEGIN-STUDENTS-TO-ADD-CODE
+    // arrange
+    beforeEach();
+    insert_node(anchor, seb);
+    insert_node(anchor, olivia);
+    insert_node(anchor, fabia);
 
-	// act
-	CU_FAIL("missing test");
-	
-	// assert
-	
-	// END-STUDENTS-TO-ADD-CODE
+    // act
+    int result = clear_list(anchor);
+
+    // assert
+    CU_ASSERT_EQUAL(result, 1);
+    CU_ASSERT_EQUAL(anchor->next, anchor);
+
+    // END-STUDENTS-TO-ADD-CODE
 }
 
 /**
  * @brief Registers and runs the tests.
  * @returns success (0) or one of the CU_ErrorCode (>0)
  */
-int main(void)
-{
+int main(void) {
     // setup, run, teardown
-    TestMainBasic("lab test", setup, teardown
-                  , test_person_compare
-                  , test_list_insert
-                  , test_list_remove
-                  , test_list_clear
-                  );
+    TestMainBasic("lab test", setup, teardown, test_person_compare, test_list_insert, test_list_remove, test_list_clear
+    );
 }
